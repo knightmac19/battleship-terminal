b1210226537784d97dc3ff667212daeb9b180a3c
Patrick Dunn <pmdunn78@gmail.com>
2024-01-22 19:39:26 -0500

    part-1 generating board

--- part-1.js
+++ part-1.js
@@ -1,1 +1,19 @@
 const readlineSync = require("readline-sync");
+
+const generateBoard = () => {
+  const board = [];
+  for (let i = 0; i < 3; i++) {
+    board.push(Array(3).fill("O"));
+  }
+  return board;
+};
+
+const displayBoard = (board) => {
+  console.log("\n  A B C");
+  for (let i = 0; i < board.length; i++) {
+    console.log(i + 1, board[i].join(" "));
+  }
+  return "";
+};
+
+console.log(displayBoard(generateBoard()));
