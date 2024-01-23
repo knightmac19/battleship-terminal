27627153e5ed50599673d8ca3bbbd41a765345b4
Patrick Dunn <pmdunn78@gmail.com>
2024-01-22 20:21:43 -0500

    validating input

--- part-1.js
+++ part-1.js
@@ -33,13 +33,43 @@ const init = () => {
   console.log("Press any key to start the game");
   readlineSync.keyIn();
 
-  displayBoard(generateBoard());
+  let gameBoard = generateBoard();
+
+  let shipsRemaining = 2;
 
   let shipOneCoordinates = generateCoordinates();
   let shipTwoCoordinates = generateCoordinates();
 
   console.log(shipOneCoordinates);
   console.log(shipTwoCoordinates);
+
+  while (shipsRemaining > 0) {
+    console.log(displayBoard(gameBoard));
+
+    const input = readlineSync
+      .question('Enter a location to strike (e.g. "A2": ')
+      .toUpperCase();
+
+    const col = input.charCodeAt(0) - 64;
+    const row = parseInt(input[1]);
+    console.log(`col: ${col}, row: ${row}`);
+
+    console.log(validateGuess(col, row));
+
+    checkForHit(shipOneCoordinates, col, row);
+    checkForHit(shipTwoCoordinates, col, row);
+  }
+};
+
+const validateGuess = (col, row) => {
+  return col > 0 && col <= 3 && row > 0 && row <= 3;
+};
+
+const checkForHit = (shipCoords, col, row) => {
+  if (shipCoords[0] === col && shipCoords[1] === row) {
+    return true;
+  }
+  return false;
 };
 
 init();
