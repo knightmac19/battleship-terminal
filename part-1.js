f3c241db64cd818a94a312d0a706a73eb36fa310
Patrick Dunn <pmdunn78@gmail.com>
2024-01-22 20:00:08 -0500

    generate ship coordinates

--- part-1.js
+++ part-1.js
@@ -16,4 +16,30 @@ const displayBoard = (board) => {
   return "";
 };
 
-console.log(displayBoard(generateBoard()));
+const getRandomNumber = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
+const generateCoordinates = () => {
+  let coordinates = [];
+  for (let i = 0; i < 2; i++) {
+    coordinates.push(getRandomNumber(0, 3));
+  }
+
+  return coordinates;
+};
+
+const init = () => {
+  console.log("Press any key to start the game");
+  readlineSync.keyIn();
+
+  displayBoard(generateBoard());
+
+  let shipOneCoordinates = generateCoordinates();
+  let shipTwoCoordinates = generateCoordinates();
+
+  console.log(shipOneCoordinates);
+  console.log(shipTwoCoordinates);
+};
+
+init();
